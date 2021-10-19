82774f807ac1002b77d0cc90f6695b1cc6ba0820
lxm <lixuemin@realibox.com>
2021-10-19 14:20:11 +0800

    feat:0.1.6更新

--- yzcore/db/sqlalchemy_crud_base.py
+++ yzcore/db/sqlalchemy_crud_base.py
@@ -36,7 +36,7 @@ UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
 
 class OrmCRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: ModelType):
+    def __init__(self, model: Type[ModelType]):
         """
         CRUD object with default methods to Create, Read, Update, Delete (CRUD).
         **Parameters**
