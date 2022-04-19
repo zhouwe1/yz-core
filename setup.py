ba32ffa89b87f3f8fd45b9bc2883cb20a5a05bd0
lxm <lixuemin@realibox.com>
2022-04-19 17:36:02 +0800

    style:版本号修改

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="yz-core",  # Replace with your own username
-    version="0.1.7",
+    version="0.1.8",
     author="lxm",
     author_email="luoyuren13@gmail.com",
     description="An ID generator for distributed microservices",
